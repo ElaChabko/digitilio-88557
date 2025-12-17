f884c504386a7fb0b42397edfb449f9362cf0922
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2025-12-17 09:27:59 +0100

    Update blogs.ts

--- src/content/blogs.ts
+++ src/content/blogs.ts
@@ -1,12 +1,12 @@
-// src/content/blogs.ts
-
-export const post1 = {
-  id: 1,
-  slug: "nowe-technologie-spolecznosciowe-2026-2027",
-  title: "Nowe technologie społecznościowe 2026–2027",
-  excerpt:
-    "Głos, wirtualne doświadczenia i AI realnie zmieniają sposób komunikacji marek.",
-  date: "17.12.2025",
-  readTime: "8 min",
-  category: "Social Media",
-};
+export const blogPosts = [
+  {
+    id: 1,
+    slug: "nowe-technologie-spolecznosciowe-2026-2027",
+    title: "Nowe technologie społecznościowe 2026–2027",
+    excerpt:
+      "Głos, wirtualne doświadczenia i AI realnie zmieniają sposób komunikacji marek.",
+    date: "17.12.2025",
+    readTime: "8 min",
+    category: "Social Media",
+  },
+];
