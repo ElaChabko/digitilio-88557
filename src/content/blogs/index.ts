05ebb0ea1f8d3d1e62ff2daca151d8054ad8a6a0
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2025-12-17 11:05:19 +0100

    Update index.ts

--- src/content/blogs/index.ts
+++ src/content/blogs/index.ts
@@ -1,6 +1,9 @@
+// src/content/blogs/index.ts
 import { noweTechnologie2026 } from "./posts/nowe-technologie-2026";
 
-export const blogPosts = [noweTechnologie2026];
+export const blogPosts = [
+  noweTechnologie2026,
+];
 
 export const getPostBySlug = (slug: string) =>
   blogPosts.find(post => post.slug === slug);
