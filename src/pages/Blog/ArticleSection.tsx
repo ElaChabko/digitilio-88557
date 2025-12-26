985c4ee61516d867d4902138db1011a3b01848f1
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2025-12-26 19:06:14 +0100

    Update ArticleSection.tsx

--- src/pages/Blog/ArticleSection.tsx
+++ src/pages/Blog/ArticleSection.tsx
@@ -36,7 +36,7 @@ export const ArticleSection = ({ section, level = 2 }: Props) => {
   );
 })()}
 
-        <div className="space-y-4 text-base sm:text-lg text-muted-foreground leading-relaxed">
+        <div className="space-y-2 text-base sm:text-lg text-muted-foreground leading-relaxed">
           {section.blocks.map((block, i) => {
             switch (block.type) {
               case "paragraph":
