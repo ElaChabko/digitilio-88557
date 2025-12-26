94983717865fe02c1f6d905820de22964d65817b
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2025-12-26 19:40:26 +0100

    Update ArticleSection.tsx

--- src/pages/Blog/ArticleSection.tsx
+++ src/pages/Blog/ArticleSection.tsx
@@ -17,7 +17,7 @@ export const ArticleSection = ({ section, level = 2 }: Props) => {
         initial={{ opacity: 0, y: 24 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
-        className="space-y-8"
+        className="space-y-4"
       >
         {/* heading jest OPCJONALNY */}
         {section.heading && (() => {
