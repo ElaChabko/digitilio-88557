0d537280a7cf57a7180bb14c7d5070191d060fe4
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2025-12-28 12:36:13 +0100

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -60,7 +60,7 @@ export default function Blog() {
       </section>
 
       {/* POSTS */}
-      <section className="py-20 bg-background">
+      <section className="py-10 bg-background">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {posts.map((post, index) => {
