2c7c3f1f8da89a0e13f32c1110ca42c5319f666a
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2026-01-02 11:51:08 +0100

    Update ContactFormDialog.tsx

--- src/components/ContactFormDialog.tsx
+++ src/components/ContactFormDialog.tsx
@@ -5,32 +5,29 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 
+import { ContactForm } from "@/components/ContactForm";
+
 interface ContactFormDialogProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
 export const ContactFormDialog = ({ isOpen, onClose }: ContactFormDialogProps) => {
-  const GOOGLE_FORM_URL = "https://forms.gle/bPNf6bg2DYdy7qDb8";
-
   return (
-    <Dialog open={isOpen} onOpenChange={onClose}>
+    <Dialog
+      open={isOpen}
+      onOpenChange={(open) => {
+        // shadcn/ui daje open:boolean – zamykamy tylko gdy open=false
+        if (!open) onClose();
+      }}
+    >
       <DialogContent className="max-w-2xl max-h-[90vh] p-0">
         <DialogHeader className="p-6 pb-0">
           <DialogTitle>Skontaktuj się ze mną</DialogTitle>
         </DialogHeader>
-        <div className="w-full h-[70vh]">
-          <iframe
-            src={GOOGLE_FORM_URL}
-            width="100%"
-            height="100%"
-            frameBorder="0"
-            marginHeight={0}
-            marginWidth={0}
-            className="rounded-b-lg"
-          >
-            Ładowanie…
-          </iframe>
+
+        <div className="p-6 pt-4">
+          <ContactForm onSuccess={onClose} />
         </div>
       </DialogContent>
     </Dialog>
