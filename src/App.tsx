cbe859372002827a80cf183605f5886516ff4fad
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2026-08-14 11:00:18 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,17 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Blog from "./pages/Blog";
+import BlogPost from "./pages/Blog/BlogPost";
+import Wspolprace from "./pages/Wspolprace";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import CookiesPolicy from "./pages/CookiesPolicy";
 import NotFound from "./pages/NotFound";
 
 import { CookieConsent } from "@/components/CookieConsent";
 import { LazyGtag } from "@/components/LazyGtag";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import CookiesPolicy from "./pages/CookiesPolicy";
-
-import BlogPost from "./pages/Blog/BlogPost";
-
-
 
 const queryClient = new QueryClient();
 
@@ -25,21 +22,30 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      
+
       <LazyGtag />
       <CookieConsent />
-      
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/polityka-prywatnosci" element={<PrivacyPolicy />} />
-          <Route path="/polityka-cookies" element={<CookiesPolicy />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:slug" element={<BlogPost />} />
 
+          <Route path="/wspolprace" element={<Wspolprace />} />
 
+          <Route
+            path="/polityka-prywatnosci"
+            element={<PrivacyPolicy />}
+          />
 
+          <Route
+            path="/polityka-cookies"
+            element={<CookiesPolicy />}
+          />
+
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
