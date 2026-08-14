96a5f249329024266729f6d6d3767f75dad9b3b9
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2026-08-14 11:11:10 +0200

    Create types.ts

---
+++ src/content/caseStudies/types.ts
@@ -0,0 +1,14 @@
+export type CaseStudy = {
+  id: number;
+  slug: string;
+
+  title: string;
+  lead: string;
+
+  industry: string;
+  scope: string;
+  period: string;
+
+  seoTitle: string;
+  seoDescription: string;
+};
