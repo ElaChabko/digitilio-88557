24dd24bf22488b9af8837f416617d29ad6dbc855
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2026-08-25 17:26:11 +0200

    Update types.ts

--- src/content/blogs/types.ts
+++ src/content/blogs/types.ts
@@ -10,5 +10,14 @@ export type ArticleSection = {
   blocks: ArticleBlock[];
 };
 
-export type BlogPost = { id: number; slug: string; title: string; excerpt?: string; date: string; readTime: string; category: string; image: string; content: ArticleSection[];
+export type BlogPost = {
+  id: number;
+  slug: string;
+  title: string;
+  excerpt?: string;
+  date: string;
+  readTime: string;
+  category: string;
+  image: string;
+  content: ArticleSection[];
 };
