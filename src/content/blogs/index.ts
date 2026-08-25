56082918d8606067efbe90775449f0c3fe0cb727
ElaChabko <113630861+ElaChabko@users.noreply.github.com>
2026-08-25 17:23:40 +0200

    Update index.ts

--- src/content/blogs/index.ts
+++ src/content/blogs/index.ts
@@ -1,10 +1,8 @@
 import { BlogPost } from "./types";
 import { noweTechnologie2026 } from "./posts/nowe-technologie-2026";
-//import { metaSupportAutomatyzacja } from "./posts/meta-support-automatyzacja";
 
 export const blogPosts: BlogPost[] = [
   metaSupportAutomatyzacja,
-  //noweTechnologie2026,
   
 ];
 
